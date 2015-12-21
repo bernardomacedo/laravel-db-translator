4dcb8ee6fd3a6d3f6cb055de69cc4e4b6e79becc
Bernardo Sousa de Macedo <bernardo.macedo@gmail.com>
2015-12-21 11:27:11 +0000

    improved readme and DBTranslator class

--- README.md
+++ README.md
@@ -69,16 +69,37 @@ In a blade template use:
 ```php
 {{ lang('some text to translate') }}
 {{ lang(':count apple named :name|:count apples named :name', 2, ['name' => 'Bernardo']) }}
-{{ lang(':count apple named :name|:count apples named :name', ['name' => 'Bernardo'], 2) }}
+{{ lang('{0} There are no apples (:count) named :name|[1,19] There are some (:count) apples named :name|[20,Inf] There are many (:count) apples named :name', ['name' => 'Bernardo'], 2) }}
 ```
+
+## What groups are for?
+
+Sometimes you need to generate a specific translation for a context based situation. Where the same phrase or text you wish to translate, means something different in other languages.
+So, the group parameter allows you to differentiate the same translation to be translated differently depending on context.
+
+```php
+{{ lang('participations') }}                /* general group assumed */
+{{ lang('participations', 'some_group') }}  /* some_group group assumed */
+```
+
+`Note: be sure the group parameter has more than 2 characters long, so DBTRanslator does not confuse it by a language.`
 
 As long as the translating text is the first function argument, you can place the other arguments in any order.
 
 ## Translating a text
 
 ```php
-DBTranslator::translate($variable, $language);
+DBTranslator::doTranslation($variable_id, $text, $language_id, $group = 'general');
 ```
+
+DBTranslator will try to find the `$variable_id` and the `$language_id` for you if you only supply strings.
+
+```php
+DBTranslator::doTranslation('This is cool', 'Isto é cool', 'pt');
+```
+
+Sometimes you wish to create a variable for translating, adding a translation directly on a language you choose.
+So, supplying a string on $variable_id that does not exist on the `translations_variables` table, will generate a new one, adding an entry to the `translations_translated` table directly.
 
 ## Generating the translation files
 
@@ -95,8 +116,12 @@ class SomeControllerName extends BaseController
          * This will generate the language translations for all
          * translated texts in the database, and will assume
          * the original language by default.
+         * This will work on all languages active by default.
          */
-        DBTranslator::generate();
+        DBTranslator::generate(); /* will generate all translations */
+
+        DBTranslator::generate('pt'); /* will generate the Portuguese translations */
+        DBTranslator::generate(64); /* will generate the Portuguese translations based on ID */
         /**
          * Redirect or do whatever you wish after generation
          */
@@ -104,3 +129,14 @@ class SomeControllerName extends BaseController
     }
 }
 ```
+
+This will create all required files under the directory supplied on the `filesystems.php` file.
+
+resources/lang/vendor/dbtranslator/
+    - en
+        - general.php
+        - some_group.php
+    - pt
+        - general.php
+        - some_group.php
+
--- src/DBTranslator.php
+++ src/DBTranslator.php
@@ -12,17 +12,64 @@ class DBTranslator
 
     public static function hasTranslation($variable_id, $language)
     {
-
+        return Translated::VarLang($variable->id, $language->id)->first();
     }
 
-    public static function generate()
+    public static function doTranslation($variable_id, $text, $language_id, $group = 'general')
+    {
+        if (!is_integer($language_id))
+        {
+            $language_id = Languages::whereIso($language_id)->first()->id;
+        }
+        if (!is_integer($variable_id))
+        {
+            /**
+             * First lets check if the translation exists or it has been previously
+             * submitted for translation
+             */
+            if ($var = Intl::whereText($variable_id)->whereGroup($group)->first())
+            {
+                $variable_id = $var->id;
+            } else {
+                $var = new Intl;
+                $var->text = $variable_id;
+                $var->group = $group;
+                $var->md5sha1 = md5($text).sha1($text);
+                $var->save();
+                $variable_id = $var->id;
+            }
+        }
+
+        $translated = Translated::VarLang($variable_id, $language_id);
+        $translated->delete();
+
+        $trans = new Translated;
+        $trans->variable_id = $variable_id;
+        $trans->language_id = $language_id;
+        $trans->translation = $text;
+        $trans->save();
+        return true;
+    }
+
+    public static function generate($language = false)
     {
         $config = config('db-translator');
         /**
          * First we get the list of languages and iterate for each one of them
          * to get the associated translations
          */
-        $languages = Languages::whereStatus(1)->get();
+        if ($language)
+        {
+            if (is_integer($language))
+            {
+                $languages[] = Languages::find($language);
+            } else {
+                $languages = Languages::whereIso($language)->get();
+            }
+        } else {
+            $languages = Languages::whereStatus(1)->get();
+        }
+        
         /**
          * Get the list of available variables in the database
          */
@@ -48,14 +95,14 @@ class DBTranslator
         {
             foreach ($values as $group => $trans)
             {
-                $file = '<?php
-return [';
+                $file = "<?php
+return [\r\n";
                 foreach ($trans as $hash => $translated)
                 {
-                    $save_value = str_replace("'", "\'", array_values($translated)[0]);
-                    $file .= '\''.array_keys($translated)[0].'\' => \''.$save_value.'\',';
+                    $save_value = str_replace('"', '\"', array_values($translated)[0]);
+                    $file .= "    \"".array_keys($translated)[0]."\" => \"".$save_value."\",\r\n";
                 }
-                $file .= '];';
+                $file .= "];";
                 \Storage::disk($config['storage_driver'])->put($language.'/'.$group.'.php', $file);
             }
         }
